27e15f4ddcb75fb3ce3839e3708eb74002b794fb
Faizan Nasim <fnasim@geduservices.com>
2026-05-06 22:28:36 +0530

    Prepare FastAPI

--- database.py
+++ database.py
@@ -1,7 +1,8 @@
-from sqlalchemy import create_engine,text
+from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-DATABASE_URL = "mysql+pymysql://root:7532053343@localhost:3306/geduu"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
