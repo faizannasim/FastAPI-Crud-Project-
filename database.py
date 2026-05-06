b779f0ff61dd5b09762331906dc0251ce61089ea
Faizan Nasim <fnasim@geduservices.com>
2026-05-06 22:17:27 +0530

    Prepare FastAPI for Vercel

--- database.py
+++ database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine,text
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = "mysql+pymysql://root:7532053343@localhost:3306/geduu"
