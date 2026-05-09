7cbfbfd8776e76f16ad8ba454c88ff6c04cc95f0
Faizan Nasim <fnasim@geduservices.com>
2026-05-09 21:41:35 +0530

    final deployment setup

--- database.py
+++ database.py
@@ -1,9 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://root:7532053343@localhost:3306/geduu"
+DATABASE_URL = "mysql+pymysql://t6qySf4S4UrjJQm.root:kVzeBEfx50cZl815@gateway01.ap-southeast-1.prod.alicloud.tidbcloud.com:4000/test"
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"ssl": {"ssl_mode": "REQUIRED"}}
+)
 
 SessionLocal = sessionmaker(
     autocommit=False,
